309037d6f5fb461b4ea6e2ada51210b296478c51
madara123pain <161628269+madara123pain@users.noreply.github.com>
2025-08-03 07:40:29 +0500

    auto: nvim config backup on 2025-08-03 07:40:29

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,5 +1,5 @@
 -- lua/plugins/colorscheme.lua
 -- No plugin spec, just a Lua module
 vim.defer_fn(function()
-    vim.cmd [[colorscheme night-owl]]
+    vim.cmd [[colorscheme citruszest]]
 end, 50)
